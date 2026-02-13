7a4d3886cfe8ab67989960f8b0eaadf42de97caf
Suleiman Mashuhuli <suleiman.mashuhuli@abcthebank.com>
2026-02-13 17:50:39 +0300

    commt

--- extensions_backend/settings.py
+++ extensions_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+df3v)39=75)b7^61*7*xqx%d)5gi3-o=&e6bg@-6wva=@5kto'
+SECRET_KEY = os.environ.get(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
